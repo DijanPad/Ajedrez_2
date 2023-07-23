2f82d0483f9f0b002bd5b6d7ebe3327bb3c1a821
357U4RD0 <cas23890@uvg.edu.gt>
2023-07-23 15:16:22 -0600

    Nueva base
    El anterior diseño empezó a dar problemas, por lo que tocó hacerlo de nuevo.

---
+++ Chess_2/Extra.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Esta la creé para que no me de problemas en el futuro con los "Null". No hace 
+ * nada, pero si no la creaba me daba problemas
+ */
+public class Extra extends Piezas
+{
+    /**
+     * Act - do whatever the Extra wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
